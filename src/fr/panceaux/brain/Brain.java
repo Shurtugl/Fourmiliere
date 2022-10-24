be5fe9f4fd6126650ca1ff71d5b11b39e89cedc5
Shurtugl_fromENI <shurtugl@hotmail.fr>
2022-10-24 17:22:44 +0200

    Modification des table en arraylist

--- src/fr/panceaux/brain/Brain.java
+++ src/fr/panceaux/brain/Brain.java
@@ -1,6 +1,7 @@
 package fr.panceaux.brain;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 	/* un Cerveau est basiquement une liste de couches
@@ -12,7 +13,7 @@ public class Brain {
 
 	private static int nbNeurone = 5;
 	private static int nbCouche = 3;
-	public static Couche[] reseau;
+	public static ArrayList<Couche> reseau;
 	
 	public static void main(String[] args) {
 		
@@ -22,7 +23,6 @@ public class Brain {
 		System.out.println("          Bienvenue         ");
 		
 		Menu();
-
 	}
 	
 	// ----------- DECLARATION DES METHODES ----------
@@ -35,25 +35,29 @@ public class Brain {
 	 * lorsque l'objet Brain sera utilisé par un code maître
 	 */
 	public static void Menu() {
-		int saisie = 0;
+	
+		int tx = 0;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("\n  --- MENU ---");
 		System.out.println("1 - Créer un réseau de "+nbCouche+" couches, chacune de "+nbNeurone+" neurones");
 		System.out.println("2 - Afficher l'état du réseau en cours");
+		System.out.println("3 - ");
+		System.out.println("4 - ");
+		System.out.println("5 - Créer un neurone ayant "+nbNeurone+" coefficients");
+		System.out.println("6 - Afficher l'état du neurone");
 		System.out.print("\n     Entrez votre choix : ");
 		
 		do {
-			saisie = sc.nextInt();
-		} while (saisie!=1 && saisie!=2);
+			tx = sc.nextInt();
+		} while (tx!=1&&tx!=2&&tx!=3&&tx!=4&&tx!=5&&tx!=6);
 		sc.close();
 		
-		switch (saisie){
+		switch (tx){
 		case 1 : constCerveau(); break;
 		case 2 : break;
 		default : System.out.println("Oups une erreur a du arriver.");
-		}
-		
+		}	
 	}
 	
 	/* Le constructor crée un agglomérat de couches
@@ -62,10 +66,12 @@ public class Brain {
 	 * 
 	 */
 	public static void constCerveau() {
-		reseau = new Couche[nbCouche];
+		reseau = new ArrayList<Couche>();
 		System.out.println(" ** Initialisation d'un réseau neuronal **");
 		for (int i =0; i<nbCouche; i++) {
-			reseau[i].constCouche();
+
+				reseau.add(new Couche(nbNeurone));
+
 		}
 	}
 	
@@ -77,7 +83,7 @@ public class Brain {
 		System.out.println(" ** Affichage du contenu du réseau neuronal ** ");
 		for (int i =0; i<nbCouche; i++) {
 			System.out.println(" --Layer n°"+i+" : --");
-			reseau[i].impCouche();
+			reseau.get(i).impCouche();
 		}
 	}
 	
@@ -94,46 +100,47 @@ public class Brain {
 	 *			- imrimer son contenu
 	 */
 	public class Couche{
-		private Neurone[] couche;
+		private ArrayList<Neurone> couche = new ArrayList<>();
 		
 		public Double setSomme_L(int nIndex) {
 			if(0<=nIndex && nIndex<=nbNeurone) {
-				return this.couche[nIndex].getSomme();
+				return this.couche.get(nIndex).getSomme();
 			}else {
 				return null;
 			}
 		}
 		
-		public Double[] getMem_L(int nIndex) {
+		public ArrayList<Double> getMem_L(int nIndex) {
 			if(0<=nIndex && nIndex<=nbNeurone) {
-				return this.couche[nIndex].getMem();
+				return this.couche.get(nIndex).getMem();
 			}else {
 				return null;
 			}
 		}
 		
 		public void setSomme_L (Double toSomme, int nIndex) {
-			couche[nIndex].setSomme(toSomme);
+			couche.get(nIndex).setSomme(toSomme);
 		}
 		
 		public void setMem_L (Double[] toMem, int nIndex) {
-			couche[nIndex].setMem(toMem);
+			couche.get(nIndex).setMem(toMem);
 		}
 		
 		public void impCouche() {
 			for (int i = 0; i<nbNeurone; i++){
 				System.out.print("["+i+"]");
-				this.couche[i].impNeurone();
+				this.couche.get(i).impNeurone();
 			}
 		}
 		
-		public void constCouche() {
-			couche = new Neurone[nbNeurone];
-			for (int i = 0; i<nbNeurone; i++){
-				System.out.print("N"+i);
-				this.couche[i].constNeurone();
-			}
-			System.out.print(" ");
+
+		public Couche(int nbN) {
+				for (int i = 0; i<nbN; i++){
+					couche.add(new Neurone());
+					System.out.print("N"+i+" ");
+				};
+
+
 		}
 	//fin Couche
 	}
@@ -150,7 +157,7 @@ public class Brain {
 	 *			-constructor
 	 */
 	public class Neurone {
-		public Double[] mem;
+		public ArrayList<Double> mem = new ArrayList<>();
 		private Double somme = null;
 		
 		public void setSomme (Double valeur){
@@ -158,39 +165,38 @@ public class Brain {
 		}
 		
 		public void setMem (Double[] tableCoeff) {
-			this.mem=tableCoeff;
+			this.mem=(ArrayList<Double>) Arrays.asList(tableCoeff);
 		}
 		
 		public Double getSomme(){
 			return this.somme;
 		}
 		
-		public Double[] getMem() {
+		public ArrayList<Double> getMem() {
 			return this.mem;
 		}
 		
 		public void impNeurone() {
 			for (int i = 0; i<nbNeurone; i++) {
-				System.out.print(this.mem[i]);
+				System.out.print(this.mem);
 			}
 			System.out.print(" "+this.somme+" /");		
 		}
 		
-		public void constNeurone() {
-			mem = new Double[nbNeurone];
-			for (int i = 0; i<nbNeurone; i++) {
-				this.mem[i]=Math.random();
-			}
+
+		public void Neurone(int nbM) {
+				for (int i = 0; i<nbM; i++) {
+					mem.add(Math.random());
+					};
+			
 			this.somme=Math.random();
 		}
+		
+
 	//fin Neurone	
 	}
 	
-	
 
-	
-	
-	
 
 //fin programme Brain.java	
 }
